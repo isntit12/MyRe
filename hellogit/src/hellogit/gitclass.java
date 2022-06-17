f7465c94b7b1d0370d155acfb3b28861b98dfd66
isnti <isnti@DESKTOP-2CBCMA2>
2022-06-17 23:36:41 +0900

    first commit in dev branch

--- hellogit/src/hellogit/gitclass.java
+++ hellogit/src/hellogit/gitclass.java
@@ -5,6 +5,8 @@ public class gitclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello git");
+		Myhello myhello = new Myhello();
+		myhello.sayHello();
 	}
 
 }
